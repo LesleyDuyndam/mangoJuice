e87a859d77e55f7454767333c40cde87c6b1dd09
Lesley Duyndam <lesleyduyndam@gmail.com>
2014-10-04 05:04:10 +0200

    Removed body padding so section.single-post has no white space on top

--- single.php
+++ single.php
@@ -3,27 +3,28 @@
 get_header(); ?>
 <section class="single-post">
     <?php while (have_posts()) : the_post(); ?>
-
-        <section>
 	        <header>
-		        <?php echo get_the_post_thumbnail(); ?>
 
+		        <?php echo get_the_post_thumbnail(); ?>
 		        <h3 class="img-overlay"><?php the_title(); ?></h3>
+
 	        </header>
 
 	        <article>
+
 		        <?php $category = get_the_category(); ?>
 		        <h4 class="category_style"><?php echo $category[0]->cat_name; ?></h4>
 		        <?php echo the_content() ;?>
+
 	        </article>
 
 	        <footer>
+
 		        <?php if(true) : ?>
 			        <a href="<?php echo "#" ;?>">Bekijk de live website!</a>
 		        <?php endif; ?>
+		        
 	        </footer>
-
-        </section>
 
 <!--        <section>-->
 <!--            --><?php //comments_template(); ?>
