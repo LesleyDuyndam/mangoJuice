07604fc02f79ae90388ff0eafe48957dbc2e72a4
Lesley Duyndam <lesleyduyndam@gmail.com>
2014-10-08 19:15:20 +0200

    Moved script.php and styles.php to functions/register/

--- functions.php
+++ functions.php
@@ -82,76 +82,42 @@ update_option('large_size_w', 1000);
 
 
 
-/**
- * Get the excerpt ( max character count )
- *
- * @param $length
- */
-function mangoJuice_excerpt( $length ) { // Max excerpt length. Length is set in characters
-
-
-
-    global $post;
-
-    //  Get the complete excerpt
-    $text = $post->post_excerpt;
-
-
-    if ( '' == $text ) {
-
-
-        $text = get_the_content('');
-        $text = apply_filters('the_content', $text);
-        $text = str_replace(']]>', ']]>', $text);
-
-
-    }
-
-
-
-    $text = strip_shortcodes($text); // optional, recommended
-    $text = strip_tags($text); // use ' $text = strip_tags($text,'<p><a>'); ' if you want to keep some tags
-    $text = substr($text,0,$length);
-
-    $excerpt = strrpos($text, '.') ? substr($text, 0, strrpos($text, '.') + 1) : false;
-
-
-
-    if( $excerpt ) {
-
-
-        echo apply_filters('the_excerpt',$excerpt);
-
-
-    } else {
-
-
-        echo apply_filters('the_excerpt',$text);
-
-
-    }
-}
-
 
 /**
  * Register menu's and sidebar's
  */
 include_once "functions/register/menus.php";
 include_once "functions/register/sidebars.php";
+include_once "functions/register/scripts.php";
+include_once "functions/register/styles.php";
+
+
+
+
+
+
+
+
+
+
+/**
+ * Load functions
+ */
+include_once "functions/the_excerpt.php";
+
+
+
+
+
+
+
+
+
+
+
 
 /**
  * Add theme controllers
  */
 include_once "controllers/bread_crumb.php";
 
-
-/**
- * Add scripts to the document
- */
-include_once "controllers/add_scripts.php";
-
-
-/**
- * Add styles to the document
- */
-include_once "controllers/add_styles.php";
