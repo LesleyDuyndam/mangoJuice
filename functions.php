b8b819b86229f5d6db71268a434f4bc94059ae6b
Lesley Duyndam <lesleyduyndam@gmail.com>
2014-10-08 19:41:01 +0200

    Rebuild functions.php

--- functions.php
+++ functions.php
@@ -23,34 +23,33 @@ function mangoJuice_setup() {
      * Add thumbnail support
      */
     if ( function_exists( 'add_theme_support' ) ) {
-        add_theme_support( 'post-thumbnails' );
-//    set_post_thumbnail_size( 800, 400, true );
+        add_theme_support('post-thumbnails');
+
+
+        /*
+         * Adds RSS feed links to <head> for posts and comments.
+         */
+        add_theme_support('automatic-feed-links');
+
+
+        /*
+         * Switches default core markup for search form, comment form, and comments
+         * to output valid HTML5.
+         */
+        add_theme_support('html5', array('search-form', 'comment-form', 'comment-list'));
+
+
+        /*
+        * Add support for different post-formats
+        * 'aside', 'audio', 'chat', 'gallery', 'image', 'link', 'quote', 'status', 'video'
+        */
+        add_theme_support('post-formats', array(//        'aside', 'audio', 'chat', 'gallery', 'image', 'link', 'quote', 'status', 'video'
+        ));
+
+
+
     }
 
-
-
-    /*
-     * Adds RSS feed links to <head> for posts and comments.
-     */
-    add_theme_support( 'automatic-feed-links' );
-
-
-
-    /*
-     * Switches default core markup for search form, comment form, and comments
-     * to output valid HTML5.
-     */
-    add_theme_support( 'html5', array( 'search-form', 'comment-form', 'comment-list' ) );
-
-
-
-    /*
-    * Add support for different post-formats
-    * 'aside', 'audio', 'chat', 'gallery', 'image', 'link', 'quote', 'status', 'video'
-    */
-    add_theme_support( 'post-formats', array(
-//        'aside', 'audio', 'chat', 'gallery', 'image', 'link', 'quote', 'status', 'video'
-    ) );
 
 
 
@@ -82,60 +81,23 @@ update_option('large_size_w', 1000);
 
 
 
-/**
- * Get the excerpt ( max character count )
- *
- * @param $length
- */
-function mangoJuice_excerpt( $length ) { // Max excerpt length. Length is set in characters
-
-
-
-    global $post;
-
-    //  Get the complete excerpt
-    $text = $post->post_excerpt;
-
-
-    if ( '' == $text ) {
-
-
-        $text = get_the_content('');
-        $text = apply_filters('the_content', $text);
-        $text = str_replace(']]>', ']]>', $text);
-
-
-    }
-
-
-
-    $text = strip_shortcodes($text); // optional, recommended
-    $text = strip_tags($text); // use ' $text = strip_tags($text,'<p><a>'); ' if you want to keep some tags
-    $text = substr($text,0,$length);
-
-    $excerpt = strrpos($text, '.') ? substr($text, 0, strrpos($text, '.') + 1) : false;
-
-
-
-    if( $excerpt ) {
-
-
-        echo apply_filters('the_excerpt',$excerpt);
-
-
-    } else {
-
-
-        echo apply_filters('the_excerpt',$text);
-
-
-    }
-}
-
 
 /**
  * Register menu's and sidebar's
  */
 include_once "functions/register/menus.php";
 include_once "functions/register/sidebars.php";
+include_once "functions/register/styles.php";
+include_once "functions/register/scripts.php";
 
+
+
+
+include_once "functions/the_excerpt.php";
+
+
+
+/**
+ * Add theme controllers
+ */
+include_once "controllers/bread_crumb.php";
