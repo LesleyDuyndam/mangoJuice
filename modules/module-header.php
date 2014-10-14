552922ebfb1a38168e471871304ec2938ba3f0c2
Lesley Duyndam <lesleyduyndam@gmail.com>
2014-10-14 13:43:47 +0200

    Added conditional title

--- modules/module-header.php
+++ modules/module-header.php
@@ -1,45 +1,35 @@
 <!DOCTYPE html>
-
-<!--[if IE 7]>
-<html class="ie ie7" <?php language_attributes(); ?>>
-<![endif]-->
-
-<!--[if IE 8]>
-<html class="ie ie8" <?php language_attributes(); ?>>
-<![endif]-->
-    
-<!--[if !(IE 7) | !(IE 8)  ]><!-->
-<html <?php language_attributes(); ?>>
-<!--<![endif]-->
+<!--[if IE 7]><html class="ie ie7" <?php language_attributes(); ?>><![endif]-->
+<!--[if IE 8]><html class="ie ie8" <?php language_attributes(); ?>><![endif]-->
+<!--[if !(IE 7) | !(IE 8)  ]><!--><html <?php language_attributes(); ?>><!--<![endif]-->
 
 <head>
     <meta charset="<?php bloginfo( 'charset' ); ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>
-        <?php
-            wp_title( '|', true, 'right' );
-            bloginfo('name');?>
-    </title>
 
     <?php
 
+    $the_title = get_bloginfo('name');
     $the_description = get_bloginfo('description');
 
     if( is_single() ){
+        $the_title = wp_title('', false);
         $the_description = get_the_excerpt();
-//            echo "<meta name='Description' content='" . get_the_excerpt() . "'>";
     }
 
-    if( is_category() ){
+    if( is_category() ) {
+        $the_title .=  " | " . single_cat_title( '', false );
         $the_description = category_description();
-//            echo "<meta name='Description' content='" . category_description() . "'/>";
-    };
+    }
 
+    echo "<title>" . $the_title . "</title>";
     echo "<meta name='Description' content='" . $the_description . "'/>";
 
     ?>
 
+<!--    Set browser sizing -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+<!--    Pingbacks -->
     <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
     <?php wp_head(); ?>
@@ -47,6 +37,10 @@
 
 <!-- START BODY -->
 <body id="body" <?php body_class(); ?>>
+
+<!--[if lt IE 7]>
+<p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> for a secure and fast experience.</p>
+<![endif]-->
 
     <!-- START HEADER -->
     <header id="header">
@@ -62,28 +56,6 @@
                         'container' => false
                     ));
                 ?><!-- END MAIN NAVIGATION -->
-
-            <!-- START .MOBILE BUTTON -->
-            <button>
-
-                <svg version="1.1" xmlns="http://www.w3.org/2000/svg">
-
-                    <!-- START EXIT LINES -->
-                    <g id="exit">
-                        <line x1="5" y1="30.342" x2="30" y2="4.843"></line>
-                        <line x1="5" y1="4.98" x2="30" y2="30.355"></line>
-                    </g><!-- END EXIT LINES -->
-
-                    <!-- START BURGER LINES -->
-                    <g id="burger">
-                        <line x1="2" y1="27.5" x2="33" y2="27.5"></line>
-                        <line x1="2" y1="17.5" x2="33" y2="17.5"></line>
-                        <line x1="2" y1="7.5" x2="33" y2="7.5"></line>
-                    </g><!-- END EXIT LINES -->
-
-                </svg>
-
-            </button><!-- END .MOBILE BUTTON -->
         </nav>
 
     </header>
