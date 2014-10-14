fd4815d0d7922d6e33ad88d7f77ec90c6c244432
Lesley Duyndam <lesleyduyndam@gmail.com>
2014-10-14 13:46:14 +0200

    switched from developer to module

--- category.php
+++ category.php
@@ -14,7 +14,7 @@ if ( have_posts() ){
 
     while ( have_posts() ) : the_post();
 
-        get_template_part( 'modules/development', 'post');
+        get_template_part( 'modules/module', 'post');
 
     endwhile;
 ?>
