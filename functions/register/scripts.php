180d64a6e667de73d9a536e52f3bada078feb716
Lesley Duyndam <lesleyduyndam@gmail.com>
2014-10-17 04:50:55 +0200

    Build a object bridge between PHP and main.js

--- functions/register/scripts.php
+++ functions/register/scripts.php
@@ -16,17 +16,32 @@ function mangoJuice_add_scripts() {
     // Register scripts
     wp_register_script('mangoJuice_jq_device', get_stylesheet_directory_uri() . '/js/plugins/device.min.js', array(), "1.0.1", true);
     wp_register_script('mangoJuice_mailchimp', get_stylesheet_directory_uri() . '/js/mailchimp.login.js', array(), "1.0.1", true);
+    wp_register_script('mangoJuice_main', get_stylesheet_directory_uri() . '/js/main.js', array(), "1.0.1", true);
     wp_register_script('mangoJuice_hamburger', get_stylesheet_directory_uri() . '/js/hamburger.js', array(), "1.0.1", true);
-    wp_register_script('mangoJuice_main-script', get_stylesheet_directory_uri() . '/js/main.js', array(), "1.0.1", true);
 
+
+
+    /**
+     * Add php wordpress data as javascript variables
+     *
+     *
+     */
+
+    $wordpress_data = array(
+        'template_url'  => get_template_directory_uri()
+    );
+
+    wp_localize_script( 'mangoJuice_main', 'wordpress_data', $wordpress_data );
 
     // Add script
     wp_enqueue_script( 'mangoJuice_jq_device');
     wp_enqueue_script( 'mangoJuice_mailchimp');
     wp_enqueue_script( 'mangoJuice_hamburger');
-    wp_enqueue_script( 'mangoJuice_main-script');
+    wp_enqueue_script( 'mangoJuice_main');
 
 
 }
 
 add_action( 'wp_enqueue_scripts', 'mangoJuice_add_scripts' );
+
+
